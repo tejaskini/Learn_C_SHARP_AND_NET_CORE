930e0355980c156afeaa64977215878737c220f1
tejaskini <tejaskini35@gmail.com>
2025-10-20 00:37:12 +0530

    C# basics

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            // arithmetics operations
+            int a = 10;
+            int b = 20;
+
+            basic_2cs obj = new basic_2cs();
+            obj.some_defination();
+
+            Console.WriteLine(a + b);
+
+
+            Console.WriteLine("Enter your age");
+
+            //string username = Console.ReadLine();
+            //Console.WriteLine("Hello " + username); 
+
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are " + age + " years old");
+
+            string fname = "Tejas";
+            string lname = "Kini";
+
+            Console.WriteLine("Your name is " + fname + " " + lname);
+            Console.WriteLine("name with concat " + string.Concat(fname, " ", lname));
+
+            // iterpolation
+            Console.WriteLine($"Your name is {fname} and {lname} he aahe.");
+
+            // accesing characters in string
+            Console.WriteLine("First character in fname is " + fname[0]);
+
+            // find index of character
+            Console.WriteLine("Index of 'j' in fname is " + fname.IndexOf('j'));
+
+            //substring  location
+            int lastname = lname.IndexOf('K');
+            string get_lname = lname.Substring(lastname);
+
+            //get lastname using substring
+            Console.WriteLine("Last name using substring is " + get_lname);
+
+            if (age >= 18)
+            {
+                Console.WriteLine("You are eligible to vote");
+            }
+            else
+            {
+                Console.WriteLine("You are not eligible to vote");
+            }
+
+            // switch statement
+
+            string day = "Monday";
+            switch (day)
+            {
+                case "Monday":
+                    Console.WriteLine("Today is Monday");
+                    break;
+                case "Tuesday":
+                    Console.WriteLine("Today is Tuesday");
+                    break;
+                default:
+                    Console.WriteLine("Some other day");
+                    break;
+            }
+
+            // while loop
+            int count = 0;
+            while (count < 5)
+            {
+                count++;
+                Console.WriteLine("Count is " + count);
+            }
+
+            // do while loop
+            do
+            {
+                Console.WriteLine("Count in do while is " + count);
+                count--;
+            }while(count > 0);
+
+            // for loop
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("For loop i is " + i);
+            }
+
+            // foreach loop
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            foreach (string car in cars)
+            {
+                Console.WriteLine("Car name is " + car);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/basic_2cs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class basic_2cs
+    {
+        public void some_defination()
+        {
+            Console.WriteLine("This is a basic 2 C# class");
+        }
+
+    }
+}
